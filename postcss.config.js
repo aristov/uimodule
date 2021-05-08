d713d46e436873028f3e18d79cd124549131e915
aristov <vv.aristov@gmail.com>
2021-05-08 17:04:40 +0300

    PostCSS: edit config

--- postcss.config.js
+++ postcss.config.js
@@ -3,16 +3,16 @@ module.exports = {
     require('autoprefixer'),
     require('postcss-preset-env'),
     require('postcss-svg'),
-    require('postcss-svgo')({
+    /*require('postcss-svgo')({
       plugins : [
-        { removeViewBox : false }
-      ]
-    })
+        { removeViewBox : false },
+      ],
+    }),*/
     /*require('postcss-pxtorem')({
         rootValue: 16,
         unitPrecision: 10,
         propList : ['*'],
         selectorBlackList: ['html']
     })*/
-  ]
+  ],
 }
