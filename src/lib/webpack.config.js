f6348fd32fe59e657042251fac48c16ee1ed269f
aristov <vv.aristov@gmail.com>
2021-07-05 10:11:55 +0300

    Added webpack config for bundling htmlmodule as a UMD library

---
+++ src/lib/webpack.config.js
@@ -0,0 +1,17 @@
+const path = require('path')
+
+module.exports = {
+  mode : 'none',
+  context : __dirname,
+  entry : '.',
+  output : {
+    path : path.join(__dirname, 'dist'),
+    filename : 'htmlmodule.bundle.js',
+    library : 'htmlmodule',
+    libraryTarget : 'umd',
+    globalObject : 'this',
+  },
+  externals : {
+    window : 'window'
+  }
+}
